f3f1c802637460738d90f0d4406cde813f9a2080
mayaasaadd <maya.allam654321@gmail.com>
2025-05-05 02:11:20 +0300

    User Service Creation

--- services/users/src/main/java/com/ua07/users/services/UserService.java
+++ services/users/src/main/java/com/ua07/users/services/UserService.java
@@ -1,2 +1,15 @@
-package com.ua07.users.services;public class UserService {
+package com.ua07.users.services;
+
+import com.ua07.users.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
 }
