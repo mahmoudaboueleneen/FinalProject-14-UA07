159bcb075083a4bfa741c476c778cfdeb4f1c7b4
mahmoudaboueleneen <mahmoudaboueleneen@gmail.com>
2025-05-07 02:34:22 +0300

    Add spring cloud k8s deps & update deployment replica count & img pull policy

--- services/users/src/main/java/com/ua07/users/controllers/UserController.java
+++ services/users/src/main/java/com/ua07/users/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.ua07.users.controllers;
 
 import com.ua07.users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -13,6 +15,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService){
         this.userService=userService;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello from User Service";
     }
 
 }
