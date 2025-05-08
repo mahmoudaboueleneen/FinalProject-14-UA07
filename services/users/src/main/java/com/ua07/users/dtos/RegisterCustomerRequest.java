a0293151a285ec987a22896348166db36208769e
mayaasaadd <maya.allam654321@gmail.com>
2025-05-08 07:01:29 +0300

    Customer Register Request Class

--- services/users/src/main/java/com/ua07/users/dtos/RegisterCustomerRequest.java
+++ services/users/src/main/java/com/ua07/users/dtos/RegisterCustomerRequest.java
@@ -1,2 +1,21 @@
-package com.ua07.users.dtos;public class RegisterCustomerRequest {
+package com.ua07.users.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterCustomerRequest {
+    private String email;
+    private String phone;
+    private String password;
+    private String fullName;
+
+    private String shippingAddress;
+    private String billingAddress;
 }
