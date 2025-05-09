e37dbd9bb40d8d49ef16ed42b7550f006b67c844
Ibrahim-Soltan <ibrahim.soltan918@gmail.com>
2025-05-10 01:00:08 +0300

    removing /api/ from notification routes

--- services/notifications/src/main/java/com/ua07/notifications/controllers/NotificationController.java
+++ services/notifications/src/main/java/com/ua07/notifications/controllers/NotificationController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("notifications")
 public class NotificationController {
 
     @Autowired
--- services/notifications/src/main/java/com/ua07/notifications/controllers/PreferencesController.java
+++ services/notifications/src/main/java/com/ua07/notifications/controllers/PreferencesController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/preferences")
+@RequestMapping("preferences")
 public class PreferencesController {
 
     @Autowired
