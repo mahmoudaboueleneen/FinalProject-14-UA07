3b8afef3efb8935bd8050f0be560e387e11b687d
Ahmed Hassaballah <ahmedhassaballah727@gmail.com>
2025-05-09 23:35:51 +0300

    updates 2

--- services/search/src/main/java/com/ua07/search/client/MerchantClient.java
+++
@@ -1,7 +0,0 @@
-package com.ua07.search.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "merchant-service", url = "${merchant.service.url}")
-public interface MerchantClient {
-}
---
+++ services/search/src/main/java/com/ua07/search/client/ProductClient.java
@@ -0,0 +1,17 @@
+package com.ua07.search.client;
+
+import com.ua07.merchants.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "merchants-service", url = "http://merchants-service:8084")
+public interface ProductClient {
+    @GetMapping("/products")
+    List<Product> getAllProducts();
+
+    @GetMapping("/products/search")
+    List<Product> searchProductsByKeyword(@RequestParam("keyword") String keyword);
+}
