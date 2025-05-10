d3931d8b5a935dda1ac00adc37c23b606dae4f8b
mahmoudaboueleneen <mahmoudaboueleneen@gmail.com>
2025-05-10 06:19:59 +0300

    [Infra] K8s full deployment

--- services/merchants/src/main/java/com/ua07/merchants/client/OrderClient.java
+++ services/merchants/src/main/java/com/ua07/merchants/client/OrderClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "transactions-service", url = "http://transactions-service:8084/orders")
+@FeignClient(name = "transactions-service", url = "${transactions.service.url}")
 public interface OrderClient {
     @GetMapping("/confirmed")
     List<Map<String,Object>> getConfirmedOrders(@RequestParam String startDate, @RequestParam String endDate);
