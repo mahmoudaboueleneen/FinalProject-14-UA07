90d57751811258edc8964c62614835d607463d84
mahmoudaboueleneen <mahmoudaboueleneen@gmail.com>
2025-05-13 02:24:48 +0300

    .

--- services/transactions/src/main/java/com/ua07/transactions/service/InvoiceService.java
+++ services/transactions/src/main/java/com/ua07/transactions/service/InvoiceService.java
@@ -1,26 +1,14 @@
 package com.ua07.transactions.service;
 
 import com.ua07.transactions.model.Order;
-import com.ua07.transactions.model.OrderLineItem;
-import com.ua07.transactions.model.OrderStatus;
-import com.ua07.transactions.model.PaymentMethod;
-import com.ua07.transactions.model.Transaction;
-import com.ua07.transactions.model.TransactionStatus;
-
-import com.ua07.transactions.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 @Service
