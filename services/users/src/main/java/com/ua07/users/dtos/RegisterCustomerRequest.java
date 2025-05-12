c5a9807a806edbd02cf9e646a2c77c08a51c4024
mayaasaadd <maya.allam654321@gmail.com>
2025-05-13 01:21:50 +0300

    Add Validation Annotations to RegisterCustomerRequest DTO

--- services/users/src/main/java/com/ua07/users/dtos/RegisterCustomerRequest.java
+++ services/users/src/main/java/com/ua07/users/dtos/RegisterCustomerRequest.java
@@ -1,20 +1,33 @@
 package com.ua07.users.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterCustomerRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid phone number format")
     private String phone;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "Full name is required")
     private String fullName;
 
+    @NotBlank(message = "Shipping address is required")
     private String shippingAddress;
+
+    @NotBlank(message = "Billing address is required")
     private String billingAddress;
 }
